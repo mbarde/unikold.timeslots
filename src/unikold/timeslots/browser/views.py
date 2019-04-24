f509415c2762f2b880f2c26249ee45d7b49e5010
Matthias Barde <mbarde@uni-koblenz.de>
2019-04-24 21:02:26 +0200

    choose timeslot view for signup sheet

--- src/unikold/timeslots/browser/views.py
+++ src/unikold/timeslots/browser/views.py
@@ -5,8 +5,15 @@ from plone.dexterity.browser.view import DefaultView
 
 class UTSignupSheetView(DefaultView):
 
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
     def isCurrentUserLoggedIn(self):
         return not api.user.is_anonymous()
 
     def showEditLinks(self):
-        return False
+        return api.user.has_permission('unikold.timeslots: Manage Schedule')
+
+    def renderExtraForm(self):
+        return ''
