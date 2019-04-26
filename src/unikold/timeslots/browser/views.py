36f81869d94c62aaf82ad7fcdb196e7b7335a3e2
Matthias Barde <matthiasbarde@gmx.de>
2019-04-26 09:47:29 +0200

    add JS

--- src/unikold/timeslots/browser/views.py
+++ src/unikold/timeslots/browser/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from plone import api
 from plone.dexterity.browser.view import DefaultView
+from Products.CMFPlone.resources import add_resource_on_request
 from Products.Five import BrowserView
 
 
@@ -9,6 +10,11 @@ class UTSignupSheetView(DefaultView):
     def __init__(self, context, request):
         self.context = context
         self.request = request
+
+    def __call__(self):
+        # load JS resources
+        add_resource_on_request(self.request, 'unikold.timeslots')
+        return super(UTSignupSheetView, self).__call__()
 
     def showEditLinks(self):
         return api.user.has_permission('unikold.timeslots: Manage Schedule')
@@ -29,6 +35,8 @@ class ShowReservationsView(BrowserView):
                 self.context.absolute_url()
                 + '/login_form?came_from=./@@show-reservations')
         else:
+            # load JS resources
+            add_resource_on_request(self.request, 'unikold.timeslots')
             return super(ShowReservationsView, self).__call__()
 
 
